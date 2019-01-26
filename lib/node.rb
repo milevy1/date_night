275968c285f0b25cacb545a414bad8613f4fbbb8
Matt Levy <milevy1@gmail.com>
2019-01-26 07:43:11 -0700

    setup an example binary search tree

--- lib/node.rb
+++ lib/node.rb
@@ -3,9 +3,30 @@ class Node
   attr_accessor :left, :right
 
   def initialize(value = nil)
-    @valiue = value
+    @value = value
     @left = nil
     @right = nil
+  end
+
+end
+
+class BinarySearchTree
+  attr_accessor :root_node
+
+  def initialize(root_value = nil)
+    @root_node = Node.new(root_value)
+  end
+
+  def insert(node, value)
+    if node.value == value
+      return node
+    elsif value < node.value
+      insert(node.left, value)
+    elsif value > node.value
+      insert(node.right, value)
+    else
+      return node = Node.new(value)
+    end
   end
 
 end
