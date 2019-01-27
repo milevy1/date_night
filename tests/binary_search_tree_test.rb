4efdf4e405e8079e9589f469373159cad79f697b
Matt Levy <milevy1@gmail.com>
2019-01-26 17:53:32 -0700

    Refactored BST test with instance variables

--- tests/binary_search_tree_test.rb
+++ tests/binary_search_tree_test.rb
@@ -5,14 +5,14 @@ require './lib/binary_search_tree'
 class BinarySearchTreeTest < Minitest::Test
 
   def setup
-    tree = BinarySearchTree.new
+    @tree = BinarySearchTree.new
   end
 
   def test_insert_movie_nodes
-    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
-    assert_equal 1, tree.insert(16, "Johnny English")
-    assert_equal 1, tree.insert(92, "Sharknado 3")
-    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+    assert_equal 0, @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, @tree.insert(16, "Johnny English")
+    assert_equal 1, @tree.insert(92, "Sharknado 3")
+    assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
   end
 
 end
